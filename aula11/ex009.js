cb9138be7b1c3732b4fe04694b322f67a539f47e
“Victor <victorvf11@outlook.com>
2024-01-25 10:20:20 -0300

    foreign x brazilian

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,7 +1,22 @@
-var país = "EUA";
-console.log(`Vivendo em ${país}`);
-if (país != "Brasil") {
-  console.log("Você é Estrangeiro");
-} else {
-  console.log("Você é Brasileiro");
+function conferir() {
+  var p1 = document.getElementById("p1");
+  var nac = document.getElementById("nac");
+
+  var confNac = String(p1.value);
+
+  nac.innerHTML = `<p>O país que você está é <strong>${confNac}</strong>. </p>`;
+
+  if (confNac !== "Brasil") {
+    nac.innerHTML += `<p>E você é Estrangeiro.</p>`;
+  } else {
+    nac.innerHTML += `<p>E você é Brasileiro.</p>`;
+  }
 }
+
+// var país = "EUA";
+// console.log(`Vivendo em ${país}`);
+// if (país != "Brasil") {
+//   console.log("Você é Estrangeiro");
+// } else {
+//   console.log("Você é Brasileiro");
+// }
