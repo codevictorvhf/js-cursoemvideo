bfcf1c9915dc17a9ae24d36bbe4110c03ea29f3f
“Victor <victorvf11@outlook.com>
2024-01-26 14:28:06 -0300

    .

--- aula12ex/ex014/model.js
+++ aula12ex/ex014/model.js
@@ -1,19 +1,22 @@
 function carregar() {
-  var msg = document.getElementById("msg");
-  var img = document.getElementById("photo");
+  let msg = document.getElementById("msg");
+  let img = document.getElementById("image");
 
-  var data = new Date();
-  // let hour = data.getHours();
-  var hour = 8;
+  let data = new Date();
+  let hour = data.getHours();
+
   msg.innerHTML = `Agora são ${hour} horas`;
-  if (hour >= 0 && hour < 12) {
+  if (hour > 0 && hour < 12) {
     // BOM DIA
     img.src = "manha.png";
-  } else if (hour >= 12 && hour < 18) {
+    document.body.style.background = "#FDDC8E";
+  } else if (hour >= 12 && hour <= 18) {
     // BOA TARDE
     img.src = "tarde.png";
+    document.body.style.background = "#F07600";
   } else {
     // BOA NOITE
     img.src = "noite.png";
+    document.body.style.background = "#000A13";
   }
 }
