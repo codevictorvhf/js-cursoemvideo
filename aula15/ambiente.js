dc26ab70911039f8375f0ab5cdcfc354eab61b1f
“Victor <victorvf11@outlook.com>
2024-01-31 16:38:53 -0300

    amb.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,3 +1,3 @@
 let num = [5, 9, 9, 2, 7];
-console.log(`O primeiro valor do vetor é ${num[1]}`);
+console.log(`O primeiro valor do vetor é ${num[0]}`);
 console.log(`O vetor tem ${num.length} posições`);
