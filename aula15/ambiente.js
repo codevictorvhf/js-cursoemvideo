f78b743511ac0d634af20ec14502449725e1bba6
“Victor <victorvf11@outlook.com>
2024-01-31 20:45:40 -0300

    condicional co vetor e reptição

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,3 +1,10 @@
 let num = [5, 9, 9, 2, 7];
 console.log(`O primeiro valor do vetor é ${num[0]}`);
 console.log(`O vetor tem ${num.length} posições`);
+
+let pos = num.indexOf(1);
+if (pos == -1) {
+  console.log("O valor não foi encontrado");
+} else {
+  console.log(`O valor está na posição ${pos}`);
+}
