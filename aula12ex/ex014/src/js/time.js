3bfb083c69a5ee0e46c092ee1a811f8f4c7170f7
“Victor <victorvf11@outlook.com>
2024-01-31 13:02:58 -0300

    att pastas

--- aula12ex/ex014/src/js/time.js
+++ aula12ex/ex014/src/js/time.js
@@ -7,13 +7,13 @@ function carregar() {
 
   msg.innerHTML = `Now it's ${hour} time`;
   if (hour > 0 && hour < 12) {
-    img.src = "manha.png";
+    img.src = "src/images/manha.png";
     document.body.style.background = "#FDDC8E";
   } else if (hour >= 12 && hour <= 18) {
-    img.src = "tarde.png";
+    img.src = "src/images/tarde.png";
     document.body.style.background = "#F07600";
   } else {
-    img.src = "noite.png";
+    img.src = "src/images/noite.png";
     document.body.style.background = "#000A13";
   }
 }
